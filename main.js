3f57394e8a2bfb52656a06b6949b874a995de695
Crezycraftuz <crezycraft0@gmail.com>
2024-11-27 22:39:17 +0500

    Create all files to github to my chanel

---
+++ main.js
@@ -0,0 +1,47 @@
+let btn = document.querySelector(".btn");
+let list = document.querySelector(".list");
+
+let removeclass = () => {
+  list.classList.remove("listopen", "listclose");
+};
+
+btn.addEventListener("click", () => {
+  if (list.classList.contains("listopen")) {
+    list.classList.add("listclose");
+    btn.classList.remove("btnclose");
+    list.addEventListener("animationend", removeclass);
+  } else {
+    list.classList.add("listopen");
+    btn.classList.add("btnclose");
+    list.removeEventListener("animationend", removeclass);
+  }
+});
+
+// demo
+if (location.href.match("jspage.html")) {
+  const button = document.querySelector(".button");
+  const image = document.querySelector(".image");
+
+  button.addEventListener("click", () => {
+    if (image.classList.contains("changeimage")) {
+      image.classList.remove("changeimage");
+      button.textContent = "Tugmani bosing";
+    } else {
+      image.classList.add("changeimage");
+      button.textContent = "Yana bosing";
+    }
+  });
+}
+
+// copy code to clipboard
+let container = document.querySelector(".container");
+
+container.addEventListener("click", (evt) => {
+  if (evt.target.matches(".code") || evt.target.matches(".code *")) {
+    navigator.clipboard.writeText(evt.target.textContent.trim());
+    evt.target.classList.add("copied");
+    setTimeout(() => {
+      evt.target.classList.remove("copied");
+    }, 1000);
+  }
+});
